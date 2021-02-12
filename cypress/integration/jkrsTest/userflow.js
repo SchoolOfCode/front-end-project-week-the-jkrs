c0c843c434225f49ad51af28a859b225984f303e
= <beckyljones151192@gmail.com>
2021-02-12 13:06:04 +0000

    done a your sessions cypress test

--- cypress/integration/jkrsTest/userflow.js
+++ cypress/integration/jkrsTest/userflow.js
@@ -4,10 +4,14 @@ import register from "./register/register";
 import logIn from "./logIn/login";
 import introduction from "./introduction/introduction";
 import iceBreaker from "./iceBreaker/iceBreaker";
+import talkingPoints from "./talkingPoints/talkingPoints";
+import yourSessions from "./yourSessions/yourSessions";
 
 openingScreen();
 homePage();
 //register();
 logIn();
 //introduction();
-iceBreaker();
+//iceBreaker();
+//talkingPoints();
+yourSessions();
